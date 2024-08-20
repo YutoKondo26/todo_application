fc0817afec0ef8c2b24dba1e863b26a4e5c20e24
YutoKondo26 <yuutokondou26@gmail.com>
2024-08-20 21:57:26 +0900

    feat: Add ToDoリスト一覧 link to navigation and update index page layout
    - Added a new link to the navigation menu for ToDoリスト一覧 page
    - Updated the index page layout to include a button for adding tasks

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -10,9 +10,11 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
-                    <section class="text-gray-600 body-font">
-                        <div class="container px-5 py-24 mx-auto">
-                          <div class="flex flex-col text-center w-full mb-20">
+                  <section class="text-gray-600 body-font">
+                    <div class="container px-5 py-24 mx-auto">
+                      <div class="lg:w-2/3 w-full mx-auto flex flex-col text-center w-full mb-20">
+                            <a href="{{route('todos.create')}}" class="flex w-fit text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">タスクを追加</a>
+
 
                             
                           </div>
@@ -20,11 +22,10 @@
                             <table class="table-auto w-full text-left whitespace-no-wrap">
                               <thead>
                                 <tr>
-                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">Plan</th>
-                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Speed</th>
-                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Storage</th>
-                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Price</th>
-                                  <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">ステータス</th>
+                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">タスク名</th>
+                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">タグ</th>
+                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">期限</th>
                                 </tr>
                               </thead>
                               <tbody>
@@ -33,37 +34,11 @@
                                   <td class="px-4 py-3">5 Mb/s</td>
                                   <td class="px-4 py-3">15 GB</td>
                                   <td class="px-4 py-3 text-lg text-gray-900">Free</td>
-                                  <td class="w-10 text-center">
-                                    <input name="plan" type="radio">
-                                  </td>
+                           
                                 </tr>
-                                <tr>
-                                  <td class="border-t-2 border-gray-200 px-4 py-3">Pro</td>
-                                  <td class="border-t-2 border-gray-200 px-4 py-3">25 Mb/s</td>
-                                  <td class="border-t-2 border-gray-200 px-4 py-3">25 GB</td>
-                                  <td class="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$24</td>
-                                  <td class="border-t-2 border-gray-200 w-10 text-center">
-                                    <input name="plan" type="radio">
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td class="border-t-2 border-gray-200 px-4 py-3">Business</td>
-                                  <td class="border-t-2 border-gray-200 px-4 py-3">36 Mb/s</td>
-                                  <td class="border-t-2 border-gray-200 px-4 py-3">40 GB</td>
-                                  <td class="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$50</td>
-                                  <td class="border-t-2 border-gray-200 w-10 text-center">
-                                    <input name="plan" type="radio">
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">Exclusive</td>
-                                  <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">48 Mb/s</td>
-                                  <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3">120 GB</td>
-                                  <td class="border-t-2 border-b-2 border-gray-200 px-4 py-3 text-lg text-gray-900">$72</td>
-                                  <td class="border-t-2 border-b-2 border-gray-200 w-10 text-center">
-                                    <input name="plan" type="radio">
-                                  </td>
-                                </tr>
+                       
+                        
+                  
                               </tbody>
                             </table>
                           </div>
