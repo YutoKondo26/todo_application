7c8abf980c774f1ab45eb26a399c35009c7b0df9
YutoKondo26 <yuutokondou26@gmail.com>
2024-08-21 03:38:10 +0900

    show

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -25,26 +25,30 @@
                                     <div class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $todo->title }}</div>
                                 </div>
                                 </div>
+                                @if($todo->description)
                                 <div class="p-2 w-full">
                                 <div class="relative">
                                     <label for="discription" class="leading-7 text-sm text-gray-600">詳細</label>
                                     <div class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{ $todo->description }}</div>
                                 </div>
                                 </div>
+                                @endif
                                 <div class="p-2 w-full">
                                 <div class="relative">
                                     <label for="deadline" class="leading-7 text-sm text-gray-600">締切</label>
                                     <div class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $todo->deadline }}</div>
                                 </div>
                                 </div>
+                                @if($todo->tag)
                                 <div class="p-2 w-full">
                                 <div class="relative">
                                     <label for="tag" class="leading-7 text-sm text-gray-600">タグ</label>
                                     <div class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $todo->tag }} </div>
                                 </div>
                                 </div>
+                                @endif
                                 <div class="flex justify-between p-2 w-full">
-                                <a href="{{ route('todos.edit', [ 'id' => $todo->id ]) }}" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集</a>
+                                <a href="{{ route('todos.edit', [ 'id' => $todo->id ]) }}" class="flex text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集</a>
                                 <form method="post" action="{{ route('todos.destroy', [ 'id' => $todo->id ]) }}">
                                     @csrf
                                     @method('DELETE')
