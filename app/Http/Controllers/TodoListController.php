a80f603ed48288537f8023c0e7dd58a6654d8e88
YutoKondo26 <yuutokondou26@gmail.com>
2024-08-20 22:34:45 +0900

    chore: Add create todo view and update navigation and index page layout

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -23,7 +23,8 @@ class TodoListController extends Controller
      */
     public function create()
     {
-        //
+            
+        return view('todos.create');
     }
 
     /**
---
+++ resources/views/todos/create.blade.php
@@ -0,0 +1,59 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            ToDo新規追加
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
+                    <section class="text-gray-600 body-font relative">
+                        <div class="container px-5 py-24 mx-auto">
+                          <div class="lg:w-1/2 md:w-2/3 mx-auto flex flex-col text-center w-full mb-12">
+
+                            
+                          </div>
+                          <form post="post" action="{{route('todos.store')}}">
+                              <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                                <div class="-m-2">
+                                  <div class="p-2 w-full">
+                                    <div class="relative">
+                                      <label for="title" class="leading-7 text-sm text-gray-600">タスク名</label>
+                                      <input type="text" id="title" name="title" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    </div>
+                                  </div>
+                                  <div class="p-2 w-full">
+                                    <div class="relative">
+                                      <label for="discription" class="leading-7 text-sm text-gray-600">Message</label>
+                                      <textarea id="discription" name="discription" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                    </div>
+                                  </div>
+                                  <div class="p-2 w-full">
+                                    <div class="relative">
+                                      <label for="deadline" class="leading-7 text-sm text-gray-600">Email</label>
+                                      <input type="date" id="deadline" name="deadline" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    </div>
+                                  </div>
+                                  <div class="p-2 w-full">
+                                    <div class="relative">
+                                      <label for="tag" class="leading-7 text-sm text-gray-600">Email</label>
+                                      <input type="text" id="tag" name="tag" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    </div>
+                                  </div>
+                                  <div class="p-2 w-full">
+                                    <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録</button>
+                                  </div>
+                                </div>
+                              </div>
+                          </form>
+                        </div>
+                      </section>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
